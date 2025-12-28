cefd6e678a97f080f00f61c37b8e2a5910272e8d
ClearTranquil <110697932+ClearTranquil@users.noreply.github.com>
2025-12-28 12:28:41 -0500

    initial setup

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("L Mouse Pressed");
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Hit: " + hit.transform.name);
+                // Perform actions on the hit object here
+            }
+        }
+    }
+}
