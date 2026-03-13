f665efcd89167556553ac4daed54e5597c4d6439
ClearTranquil <110697932+ClearTranquil@users.noreply.github.com>
2026-03-13 10:10:50 -0400

    Customer Animations
    - Simple bobbing animations for customers to give the scene a little more life

--- Assets/Scripts/NPCs/Customer.cs
+++ Assets/Scripts/NPCs/Customer.cs
@@ -7,12 +7,22 @@ public class Customer : MonoBehaviour, ICustomerInfo
     [Range(0f, 1f)]
     [SerializeField] private float baseTipChance = 0.1f;
 
-    [SerializeField] private Transform cupSlot;
-
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 2f;
-
+    [SerializeField] private float arrivalThreshold = 0.05f;
     private Transform target;
+
+    [SerializeField] private Transform cupSlot;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+
+        // Offset idle anim so customers don't move in sync
+        float randomOffset = Random.value;
+        animator.Play("idle", 0, randomOffset);
+    }
 
     /*==========Customer Data============*/
     public float BaseTipChance => baseTipChance;
@@ -27,12 +37,16 @@ public class Customer : MonoBehaviour, ICustomerInfo
     public void MoveTo(Transform newTarget)
     {
         target = newTarget;
+
+        animator.SetBool("walking", true);
     }
 
     public void TeleportTo(Transform newTarget)
     {
         target = null;
         transform.position = newTarget.position;
+
+        animator.SetBool("walking", true);
     }
 
     private void Update()
@@ -41,10 +55,16 @@ public class Customer : MonoBehaviour, ICustomerInfo
             return;
 
         transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, target.position) <= arrivalThreshold)
+        {
+            target = null;
+            animator.SetBool("walking", false);
+        }
     }
 
     /*============Customer Logic============*/
-     
+
     public void ReceiveCup(Cup cup, float moveTime = 0.5f)
     {
         StartCoroutine(MoveCupToHand(cup, moveTime));
